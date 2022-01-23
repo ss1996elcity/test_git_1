659bea2712a070b4b266e0f9c818a48ed6d21981
Sergey SS <Dor7la96@mail.ru>
2022-01-23 18:41:01 +0300

    check 3

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
+import "fmt"
 import "git_project/pkg1"
 import "git_project/pkg2"
 
 func main() {
 
-  fmt.Prinln("My programm with git")
+  fmt.Println("My programm with git")
   pkg1.F1()
   pkg2.F2()
 
