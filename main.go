bfd4c58945b8e3d44c0b46d463bcffb31308045e
Sergey SS <Dor7la96@mail.ru>
2022-01-24 11:52:21 +0300

    year

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 import "git_project/pkg1"
 import "git_project/pkg2"
-
+import "git_project/nb_pkg"
 func main() {
   fmt.Println("My programm with git")
   pkg1.F1()
   pkg2.F2()
   fmt.Println("Ululululuu")
   fmt.Println("Blablabla_Ululululu")
-
+  nb_pkg.F3()
 }
